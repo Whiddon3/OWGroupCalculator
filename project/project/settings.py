284e0292852db0b8ff9d23f0d0db05901f988d6d
Whiddon Sibdhannie <whiddon3@gmail.com>
2023-03-17 00:36:39 -0400

    Read secret key to .env

--- project/project/settings.py
+++ project/project/settings.py
@@ -12,15 +12,26 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+import dotenv # <- New
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Add .env variables anywhere before SECRET_KEY
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+# UPDATE secret key
+SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uf_@^e6p2i$-)!zczlw9c2s@aba#lgl5qf0o3f-b_=t%y*um=9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
